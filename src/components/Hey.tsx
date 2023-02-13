b85b74fe63cae20116875cc092d374afda6dd579
Josh Barnett <oh@heyjo.sh>
2023-02-13 01:22:21 -0500

    Hello World

---
+++ src/components/Hey.tsx
@@ -0,0 +1,7 @@
+import { Component } from 'solid-js';
+
+export const Hey: Component = () => (
+  <div class='flex flex-row gap-6 items-center justify-center mt-12 text-6xl font-bold'>
+    <h1>Hi 👋 I&rsquo;m Josh.</h1>
+  </div>
+);
